1b9e2489041949191bc88a9bab70106575d27fe4
github_ws_shimada <137061803+adashino23sky@users.noreply.github.com>
2025-04-16 21:20:32 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+# import
+import os
+import streamlit as st
+import streamlit-chat
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+# ページ切り替え関数
+def go_to(page_name):
+    st.session_state.page = page_name
+
+def home():
+    st.title("ホームページ")
+    if st.button("設定ページへ"):
+        go_to("settings")
+    return 
+
+def setting_prompt():
+
+
+
+def main():
+    st.title("ホームページ")
+    if st.button("設定ページへ"):
+        go_to("settings")
+    return 
+
+
+
+
+
+
+
+
+
+    st.markdown('# 画像を保存するデモ')
+    file = st.file_uploader('画像をアップロードしてください.', type=['jpg', 'jpeg', 'png'])
+    if file:
+        st.markdown(f'{file.name} をアップロードしました.')
+        img_path = os.path.join(IMG_PATH, file.name)
+        # 画像を保存する
+        with open(img_path, 'wb') as f:
+            f.write(file.read())
+            
+        # 保存した画像を表示
+        img = Image.open(img_path)
+        st.image(img)
+
+if __name__ == '__main__':
+    main()
